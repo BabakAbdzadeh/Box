78f225b1f133e66dbe0aab25d6392157b28ef885
Babak Abdolah Zadeh <babak.abdzadeh@gmail.com>
2022-11-30 01:08:38 +0100

    Ready for DataBase

--- app.js
+++ app.js
@@ -62,65 +62,68 @@ const inputObject = {
 
 };
 
-const sumObject = {
-  boxSum : 0,
-  contributorsSum: {
-    'babak' : 0,
-    'pedro': 0,
-    'madre': 0,
-    'sahand': 0,
-  }
-}
-
-
-
-
-
-
-
+// const sumObject = {
+//   boxSum : 0,
+//   contributorsSum: {
+//     'babak' : 0,
+//     'pedro': 0,
+//     'madre': 0,
+//     'sahand': 0,
+//   }
+// }
 
 const express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
+const queryString = require('querystring');
 // create application/json parser
 app.use(express.json());
 app.use(express.urlencoded({
   extended: false
 }));
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
 // app.use(jsonParser);
 
 app.get('/', (req, res) => {
   // JUSt doing the code here for easy access
-  for(var index in inputObject){
-    let contributorsObj = inputObject[index].contributors;
-    let productPrice = parseFloat(inputObject[index].product.price);
-    let eachPersonCut = productPrice/ Object.keys(contributorsObj).length ;
-    sumObject.boxSum += productPrice;
-    // Loop through contributors:
-    for(var person in contributorsObj){
-      console.log(person);
-      // parseInt becuase its integer -> Later Update
-      contributorsObj[person] = parseFloat(contributorsObj[person]) - eachPersonCut;
-      if(sumObject.contributorsSum[person] !== undefined){
-        console.log(`Its gonna (+/-) ${parseFloat(contributorsObj[person])} in ${sumObject.contributorsSum[person]}`);
-        sumObject.contributorsSum[person] += parseFloat(contributorsObj[person]);
 
-      }
-    }
+  if(req.query.valid !== false){
+    console.log(req.query)
+  }else{
+    console.log("this one is false : " + req.query.valid);
   }
-  console.log(inputObject);
-  console.log(sumObject);
-  // Code finished
-
 
   res.sendFile(__dirname + '/public/html/home.html');
-})
+});
+
 app.post('/', (req, res) => {
   const inputObject = req.body;
+
   // Logic
+  // 1) First Create sumObject
+  let contributorsNamesArr = [];
   for(var index in inputObject){
     let contributorsObj = inputObject[index].contributors;
+
+    if(contributorsNamesArr.length < Object.keys(contributorsObj).length){
+      contributorsNamesArr = Object.keys(contributorsObj);
+    }};
+    let sumObject = {
+      boxSum : 0,
+      contributorsSum : {
+      }
+    }
+    contributorsNamesArr.forEach(name => {
+      sumObject.contributorsSum[name] = 0;
+    });
+  // 2) Calculation and save data in sumObject
+  for(var index in inputObject){
+    let contributorsObj = inputObject[index].contributors;
+
+    if(contributorsNamesArr.length < Object.keys(contributorsObj).length){
+      contributorsNamesArr = Object.keys(contributorsObj);
+    }
     let productPrice = parseFloat(inputObject[index].product.price);
     let eachPersonCut = productPrice/ Object.keys(contributorsObj).length ;
     sumObject.boxSum += productPrice;
@@ -139,10 +142,15 @@ app.post('/', (req, res) => {
   console.log(inputObject);
   console.log(sumObject);
   // Logic finish
-  res.send(sumObject);
+//  Ready For DATABASE and redirect
+
+
 });
 
 
 app.listen(3000, () => {
   console.log("connected to port 3000");
-})
+});
+
+
+// Functions
