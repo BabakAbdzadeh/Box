428a05fd66a4e29ca79b724ebdc4fb5efb6dc122
Babak Abdolah Zadeh <babak.abdzadeh@gmail.com>
2022-12-05 20:57:21 +0100

    BackEnd Calculates + add data to DB

--- app.js
+++ app.js
@@ -1,66 +1,66 @@
-const inputObject = {
-  0: {
-    contributors: {
-      'babak': '10',
-      'pedro': '20',
-      'madre': '0',
-      'sahand': '0'
-    },
-    product: {
-      name: 'milk',
-      price: '30'
-    }
-  },
-  1: {
-    contributors: {
-      'babak': '40',
-      'pedro': '0',
-      'madre': '0',
-      'sahand': '0'
-    },
-    product: {
-      name: 'orange',
-      price: '40'
-    }
-  },
-  2: {
-    contributors: {
-      'babak': '0',
-      'pedro': '20',
-      'madre': '0',
-      'sahand': '0'
-    },
-    product: {
-      name: 'bread',
-      price: '20'
-    }
-  },
-  3: {
-    contributors: {
-
-      'pedro': '0',
-      'madre': '30',
-      'sahand': '0'
-    },
-    product: {
-      name: 'fish',
-      price: '30'
-    }
-  },
-  4: {
-    contributors: {
-      'babak': '0',
-      'pedro': '0',
-      'madre': '0',
-      'sahand': '100'
-    },
-    product: {
-      name: 'benzin',
-      price: '100'
-    }
-  }
-
-};
+// const inputObject = {
+//   0: {
+//     contributors: {
+//       'babak': '10',
+//       'pedro': '20',
+//       'madre': '0',
+//       'sahand': '0'
+//     },
+//     product: {
+//       name: 'milk',
+//       price: '30'
+//     }
+//   },
+//   1: {
+//     contributors: {
+//       'babak': '40',
+//       'pedro': '0',
+//       'madre': '0',
+//       'sahand': '0'
+//     },
+//     product: {
+//       name: 'orange',
+//       price: '40'
+//     }
+//   },
+//   2: {
+//     contributors: {
+//       'babak': '0',
+//       'pedro': '20',
+//       'madre': '0',
+//       'sahand': '0'
+//     },
+//     product: {
+//       name: 'bread',
+//       price: '20'
+//     }
+//   },
+//   3: {
+//     contributors: {
+//
+//       'pedro': '0',
+//       'madre': '30',
+//       'sahand': '0'
+//     },
+//     product: {
+//       name: 'fish',
+//       price: '30'
+//     }
+//   },
+//   4: {
+//     contributors: {
+//       'babak': '0',
+//       'pedro': '0',
+//       'madre': '0',
+//       'sahand': '100'
+//     },
+//     product: {
+//       name: 'benzin',
+//       price: '100'
+//     }
+//   }
+//
+// };
 
 // const sumObject = {
 //   boxSum : 0,
@@ -76,6 +76,8 @@ const express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
 const queryString = require('querystring');
+const mongoose = require('mongoose');
+const Summary = require(__dirname + '/db/db.js');
 // create application/json parser
 app.use(express.json());
 app.use(express.urlencoded({
@@ -88,9 +90,9 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
   // JUSt doing the code here for easy access
 
-  if(req.query.valid !== false){
+  if (req.query.valid !== false) {
     console.log(req.query)
-  }else{
+  } else {
     console.log("this one is false : " + req.query.valid);
   }
 
@@ -103,48 +105,64 @@ app.post('/', (req, res) => {
   // Logic
   // 1) First Create sumObject
   let contributorsNamesArr = [];
-  for(var index in inputObject){
-    let contributorsObj = inputObject[index].contributors;
-
-    if(contributorsNamesArr.length < Object.keys(contributorsObj).length){
-      contributorsNamesArr = Object.keys(contributorsObj);
-    }};
-    let sumObject = {
-      boxSum : 0,
-      contributorsSum : {
-      }
+  for (var index in inputObject) {
+    // PARSE String to MAP
+    let contributorsMap = JSON.parse(inputObject[index].contributors, reviver);
+    if (contributorsNamesArr.length < contributorsMap.size) {
+      contributorsNamesArr = Array.from(contributorsMap.keys());
     }
-    contributorsNamesArr.forEach(name => {
-      sumObject.contributorsSum[name] = 0;
-    });
+  };
+  let contributorsSumMap = new Map();
+  let sumObject = {
+    boxSum: 0,
+    contributorsSum: contributorsSumMap,
+  }
+  contributorsNamesArr.forEach(name => {
+    sumObject.contributorsSum.set(name , 0);
+  });
   // 2) Calculation and save data in sumObject
-  for(var index in inputObject){
-    let contributorsObj = inputObject[index].contributors;
+  for (var index in inputObject) {
+    // PARSE String to MAP
+    let contributorsMap = JSON.parse(inputObject[index].contributors, reviver);
 
-    if(contributorsNamesArr.length < Object.keys(contributorsObj).length){
-      contributorsNamesArr = Object.keys(contributorsObj);
-    }
     let productPrice = parseFloat(inputObject[index].product.price);
-    let eachPersonCut = productPrice/ Object.keys(contributorsObj).length ;
+    let eachPersonCut = productPrice / contributorsMap.size;
     sumObject.boxSum += productPrice;
     // Loop through contributors:
-    for(var person in contributorsObj){
-      console.log(person);
-      // parseInt becuase its integer -> Later Update
-      contributorsObj[person] = parseFloat(contributorsObj[person]) - eachPersonCut;
-      if(sumObject.contributorsSum[person] !== undefined){
-        console.log(`Its gonna (+/-) ${parseFloat(contributorsObj[person])} in ${sumObject.contributorsSum[person]}`);
-        sumObject.contributorsSum[person] += parseFloat(contributorsObj[person]);
-
+    contributorsMap.forEach((amount, person, map) => {
+      map.set(person, parseFloat(amount - eachPersonCut));
+      if (sumObject.contributorsSum.get(person) !== undefined) {
+        // Report about the correctness of the contributor's +/-
+        // console.log(`Its gonna (+/-) ${parseFloat(contributorsMap.get(person))} in ${sumObject.contributorsSum.get(person)}`);
+        sumObject.contributorsSum.set(person,  parseFloat(sumObject.contributorsSum.get(person) + contributorsMap.get(person)));
       }
-    }
+    });
+
   }
   console.log(inputObject);
   console.log(sumObject);
   // Logic finish
-//  Ready For DATABASE and redirect
+  // Talk to DB:
+  const newSummary = new Summary({
+    boxSum: sumObject.boxSum,
+    contributors: sumObject.contributorsSum,
+  });
+  newSummary.save();
+  // Redirect Phase:
+  res.json({status : 'ok'});
+
+});
 
 
+app.get('/results', (req, res) => {
+  console.log(Summary.find({}, (err, results) => {
+    if (!err) {
+      console.log('Redirect OK');
+      res.render("result");
+    } else {
+      console.log(err);
+    }
+  }));
 });
 
 
@@ -154,3 +172,12 @@ app.listen(3000, () => {
 
 
 // Functions
+// To revive stringified MAP to original dataStructure
+function reviver(key, value) {
+  if (typeof value === 'object' && value !== null) {
+    if (value.dataType === 'Map') {
+      return new Map(value.value);
+    }
+  }
+  return value;
+}
