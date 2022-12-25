9e7a0fc2f83e21e6a35669d7eb9d3242e73aaa70
Babak Abdolah Zadeh <babak.abdzadeh@gmail.com>
2022-12-25 08:21:13 +0100

    Add react files

---
+++ client/src/components/AddContributors.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+
+import _ from "lodash"
+
+
+function AddContributors() {
+    // Hooks
+    const [inputText, setInputText] = useState();
+    const [namesArray, setNames] = useState([]);
+    // Methods
+    function handleInputText(e) {
+
+        setInputText(e.target.value);
+
+    };
+    function addNames() {
+        if (inputText) {
+            setNames(prevNames => {
+                // using lodash 
+                return [...prevNames, _.startCase(inputText)]
+            })
+        }
+        setInputText("");
+    }
+    function removeName(nameIndex) {
+        setNames(prevNames => {
+            return prevNames.filter((item, index) => {
+                return index !== nameIndex;
+            })
+        })
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+    // UI
+    return (
+        <div className="contributors-container">
+            <div className="add-contributors">
+                <form onSubmit={handleSubmit}>
+                    <div className="input-and-button">
+
+                        <input type="text" pattern=".*" onChange={handleInputText} placeholder="Write names..." value={inputText} />
+
+                        <button type="button" onClick={addNames}>
+                            Add
+                        </button>
+                    </div>
+                    <div className="submit-contributors">
+                        <button type="submit">Done with adding Names</button>
+                    </div>
+                </form>
+            </div>
+            <div className="show-contributors">
+                <ul>
+                    {
+                        namesArray.map((item, index) => <li key={index} className="list-item">
+                            <h4>
+                                {item}
+                            </h4>
+
+                            <button onClick={() => { removeName(index) }}>Remove</button>
+                        </li>
+                        )
+                    }
+                </ul>
+            </div>
+
+        </div>
+
+    )
+}
+
+export default AddContributors;
---
+++ client/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AddContributors from "./AddContributors";
+
+
+
+
+function App(){
+    return (
+        <div className="app-container">
+            <AddContributors />
+        </div>
+    )
+}
+
+
+export default App;
