b733377c8b3c77b6ffaab920aaec6abbc68a8e09
Babak Abdolah Zadeh <babak.abdzadeh@gmail.com>
2022-12-26 05:10:51 +0100

    revised for new functionality

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddContributors from "./AddContributors";
+import AddProducts from './AddProducts.jsx';
 
 
 
 
-function App(){
+
+function App() {
+    const [isAddContributorsShown, setIsAddContributors] = useState(true);
+    const [JSON, setJSON] = useState({
+        names: [],
+        products: [
+            {
+                name: "",
+                price: "",
+                payers: [
+                    {
+                        name: "",
+                        amount: ""
+                    }
+                ],
+                involvedPersons: []
+            }
+        ]
+    });
+    const [namesArray, setnamesArray] = useState([]);
+    function chooseComponent(boolian) {
+        // we want false after click
+        setIsAddContributors(boolian);
+    }
+    function handleNamesArrayState(newState) {
+        setnamesArray(newState);
+    }
+
+
     return (
         <div className="app-container">
-            <AddContributors />
+            {isAddContributorsShown ? <AddContributors handleNamesArrayState={handleNamesArrayState} chooseComponent={chooseComponent} /> : <AddProducts namesArray={namesArray} />}
+
+
+
         </div>
     )
 }
