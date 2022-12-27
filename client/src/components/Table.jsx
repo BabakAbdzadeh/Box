0556404ffcfc893d6067df8fd8faa4545f83ea8e
Babak Abdolah Zadeh <babak.abdzadeh@gmail.com>
2022-12-27 02:56:04 +0100

    fix and updates: 1. user can add amount of money that paid 2. Delete people who are not envolved in the purchase 3. since the table is the child of child and probably the youngest child, it recive data based on the defined structure and play and manipulate based on tht structure

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -2,39 +2,93 @@ import React, { useState } from 'react';
 
 function Table(props) {
 
-    const [tableData, setTableData] = useState(props.tableData);
+    const tableData = props.tableData
+    let falsesArray = new Array(tableData.payers.length).fill(false);
+    const [inputDisabledState, setInputDisabledState] = useState(falsesArray);
+    const [modifiedTableData, setModifiedTableData] = useState(tableData);
 
+    function handleInputChange(e) {
+        const { id, value } = e.target;
+        const tableIndex = id;
+
+
+        setModifiedTableData(prevValue => {
+            const updatedPayers = [...prevValue.payers];
+            updatedPayers[tableIndex] = {
+                ...updatedPayers[tableIndex],
+                paid: value
+            };
+            return {
+                ...prevValue,
+                payers: updatedPayers
+            }
+        });
+    };
+
+    function checkBox(e, index) {
+        setInputDisabledState((prevState) => {
+            const newState = [...prevState];
+            newState[index] = e.target.checked;
+            return newState;
+        });
+    }
+
+    function preprationToSendBack() {
+        // remove disabled folks
+        setModifiedTableData(prevValue => {
+            const cloneOfPayers = [...prevValue.payers];
+            const updatedPayers = cloneOfPayers.filter((payer, index) => {
+                return inputDisabledState[index] === false;
+            });
+            console.log(updatedPayers);
+            return {
+                ...prevValue,
+                payers: updatedPayers
+            }
+        })
+        console.log(modifiedTableData);
+        // props.recieveData(modifiedTableData);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
 
     return (
         <div>
-
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Name</th>
-                        <th>Amount</th>
-                        <th>Check</th>
-                    </tr>
-                    <tr>
-                        <td>{tableData.name}</td>
-                        <td>
-                            <input type="number" defaultValue={tableData.price} />
-                        </td>
-                        <td>-</td>
-                    </tr>
-                    {tableData.payers.map((payer, index) =>
-                        <tr key={index}>
-                            <td>{payer}</td>
-                            <td>
-                                <input type="number" defaultValue={0} />
-                            </td>
-                            <td>
-                                <input type="checkbox" />
-                            </td>
+            <form onSubmit={handleSubmit}>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>Name</th>
+                            <th>Amount</th>
+                            <th>Not participant</th>
                         </tr>
-                    )}
-                </tbody>
-            </table>
+                        <tr>
+                            <td>{tableData.name}</td>
+                            <td>
+                                <input type="number" name="price" defaultValue={tableData.price} />
+                            </td>
+                            <td>-</td>
+                        </tr>
+                        {tableData.payers.map((payer, index) =>
+                            <tr key={index}>
+                                <td>{payer.name}</td>
+                                <td>
+                                    <input type="number" id={index} disabled={inputDisabledState[index]} name="payer.paid" placeholder="How much paid?" onChange={handleInputChange} />
+                                </td>
+                                <td>
+                                    <input type="checkbox" onChange={(e) => checkBox(e, index)} />
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+                <button onClick={() => props.deleteTable(tableData.id)}> Remove product</button>
+                <button onClick={() => {
+                    preprationToSendBack();
+                }}> Next</button>
+            </form>
 
         </div>
     );
@@ -42,3 +96,7 @@ function Table(props) {
 
 
 export default Table;
+
+
+
+// Add users paid amount.
