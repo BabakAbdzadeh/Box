0ffc773d06bb0f0ef873d7f4c02b059edf613f86
Babak Abdolah Zadeh <babak.abdzadeh@gmail.com>
2023-03-27 04:46:20 +0430

    clone the table, to keep it on the component

--- client/src/utils/pdf-print.js
+++ client/src/utils/pdf-print.js
@@ -1,27 +1,29 @@
 
 // Print the results using Window Object.
-
 function createPDF(id) {
-    const table = document.getElementById(`${id}`);
+    const table = document.getElementById(id);
+    // Create a clone of the table to print, so that we don't modify the original.
+    const tableClone = table.cloneNode(true);
+
+    const printWindow = window.open("", "", "height=700,width=700");
+    const printDocument = printWindow.document;
 
     const style = document.createElement("style");
-    style.textContent = `table {width: 90%;font: 17px Calibri;} 
-     table, th, td {border: solid 1px #DDD; border-collapse: collapse; 
-     padding: 2px 3px;text-align: center;}`
+    style.textContent =
+        "table {width: 90%;font: 17px Calibri;} " +
+        "table, th, td {border: solid 1px #DDD; border-collapse: collapse; " +
+        "padding: 2px 3px;text-align: center;}";
 
-
-    // CREATE A WINDOW OBJECT.
-    const win = window.open('', '', 'height=700,width=700');
-    const doc = win.document;
     const title = document.createElement("title");
-    title.textContent = "Profile"
-    doc.body.append(title);
-    doc.body.append(style);
-    doc.body.append(table);
-    win.print();
+    title.textContent = "Table";
 
+    printDocument.head.appendChild(style);
+    printDocument.head.appendChild(title);
+    printDocument.body.appendChild(tableClone);
 
+    printWindow.print();
 }
+
 
 
 
