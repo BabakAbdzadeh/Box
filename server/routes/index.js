a1b95d0a13bf52e1f2c3f101d3f95469af4f8f84
Babak Abdolah Zadeh <babak.abdzadeh@gmail.com>
2023-04-08 20:40:14 +0430

    CRUD

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,14 +2,18 @@ var express = require('express');
 var router = express.Router();
 const calculateController = require("../controllers/calculate");
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
+// /* GET home page. */
+// router.get('/', function (req, res, next) {
 
-  res.render('index', { title: 'Express' });
-});
+//   res.render('index', { title: 'Express' });
+// });
 
 
 router.route('/')
+  .get(calculateController.getAllResults)
   .post(calculateController.postDocument);
+router.route('/:id')
+  .put(calculateController.updateDocument)
+  .delete(calculateController.deleteDocument);
 
 module.exports = router;
