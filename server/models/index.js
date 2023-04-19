852924ec20b9609809c2473b6c2cad730890b502
Babak Abdolah Zadeh <babak.abdzadeh@gmail.com>
2023-04-19 19:16:07 +0430

    add user

--- server/models/index.js
+++ server/models/index.js
@@ -3,7 +3,9 @@ mongoose.Promise = global.Promise;
 
 const db = {};
 db.mongoose = mongoose;
-db.results = require("./result.model");
+db.User = require("./user.model");
+db.Results = require("./result.model");
+
 
 
 module.exports = db;
