05b3bf95c86acb726f9b0d9cf99cfd44e2ea02c9
Babak Abdolah Zadeh <babak.abdzadeh@gmail.com>
2023-05-02 18:43:09 +0200

    Error handler function to broaden the error handling for refreshToken

--- server/middlewares/authJwt.js
+++ server/middlewares/authJwt.js
@@ -1,6 +1,13 @@
 const jwt = require("jsonwebtoken");
 const config = require("../config/auth.config.js");
 
+const { TokenExpiredError } = jwt;
+const catchError = (err, res) => {
+    if (err instanceof TokenExpiredError) {
+        return res.status(401).send({ message: "Unauthorized! Expired Access Token" });
+    }
+    return res.sendStatus(401).send({ message: "Unauthorized!" });
+}
 
 verifyToken = (req, res, next) => {
     let token = req.headers["x-access-token"];
@@ -11,7 +18,7 @@ verifyToken = (req, res, next) => {
 
     jwt.verify(token, config.secret, (err, decoded) => {
         if (err) {
-            return res.status(401).send({ message: "Unauthorized!" });
+            return catchError(err, res);
         }
         req.userId = decoded.id;
         next();
