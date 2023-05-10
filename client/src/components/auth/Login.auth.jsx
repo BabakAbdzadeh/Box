3fe71714fd47d3568b5e572d8936cda807d11a0c
Babak Abdolah Zadeh <babak.abdzadeh@gmail.com>
2023-05-10 19:29:15 +0200

    update auth components API call works

--- client/src/components/auth/Login.auth.jsx
+++ client/src/components/auth/Login.auth.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { login } from "../../services/auth.service"
 
 
 function LoginComponent(props) {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [formSubmitted, setFormSubmitted] = useState(false);
+
+    // API call
+    useEffect(() => {
+        if (formSubmitted) {
+            login(username, password);
+        }
+    }, [formSubmitted]);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setFormSubmitted(true);
+    };
+
     return (
         <>
             <div class="auth-form-container">
                 <h2 class="login-title">Log in</h2>
 
-                <form class="login-form">
+                <form class="login-form" onSubmit={handleSubmit}>
                     <div>
                         <label for="name">Username </label>
                         <input
@@ -15,21 +33,11 @@ function LoginComponent(props) {
                             type="text"
                             placeholder="e.g. CoolGuy2023"
                             name="name"
+                            value={username}
+                            onChange={e => setUsername(e.target.value)}
                             required
                         />
                     </div>
-
-                    <div>
-                        <label for="email">Email </label>
-                        <input
-                            id="email"
-                            type="email"
-                            placeholder="me@example.com"
-                            name="email"
-                            required
-                        />
-                    </div>
-
                     <div>
                         <label for="password">Password </label>
                         <input
@@ -37,6 +45,8 @@ function LoginComponent(props) {
                             type="password"
                             placeholder="password"
                             name="password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
                             required
                         />
                     </div>
--- client/src/components/auth/Register.auth.jsx
+++ client/src/components/auth/Register.auth.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { register } from "../../services/auth.service";
 
 
 function RegisterComponent(props) {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [formSubmitted, setFormSubmitted] = useState(false);
+
+    // API call
+    useEffect(() => {
+        if (formSubmitted) {
+            register(username, email, password);
+        }
+    }, [formSubmitted]);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setFormSubmitted(true);
+    };
+
+
     return (
         <>
             <div class="auth-form-container">
                 <h2 class="login-title">Register</h2>
 
-                <form class="login-form">
+                <form class="login-form" onSubmit={handleSubmit}>
                     <div>
                         <label for="name">Username </label>
                         <input
@@ -15,6 +35,8 @@ function RegisterComponent(props) {
                             type="text"
                             placeholder="Please write your username"
                             name="name"
+                            value={username}
+                            onChange={e => setUsername(e.target.value)}
                             required
                         />
                     </div>
@@ -26,6 +48,8 @@ function RegisterComponent(props) {
                             type="email"
                             placeholder="me@example.com"
                             name="email"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
                             required
                         />
                     </div>
@@ -37,11 +61,13 @@ function RegisterComponent(props) {
                             type="password"
                             placeholder="password"
                             name="password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
                             required
                         />
                     </div>
 
-                    <button class="btn btn--form" type="submit" value="Log in">
+                    <button class="btn btn--form" type="submit" value="Register">
                         Register
                     </button>
                 </form>
