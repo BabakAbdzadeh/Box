6cbdbc559b014d053329cad098682123025efc70
Babak Abdolah Zadeh <babak.abdzadeh@gmail.com>
2023-05-11 20:55:49 +0200

    update for new features and navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,26 @@
 
 import React, { useState, useEffect } from 'react';
+
 import AddContributors from "./AddContributors";
 import AddProducts from './AddProducts.jsx';
 import Result from './Result'
+// React routing
+import AllResults from './AllResults';
+import LoginComponent from './auth/Login.auth';
+import RegisterComponent from './auth/Register.auth';
+import Profile from './user/Profile.user';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { logout } from '../services/auth.service';
 
 
 
 
 function App() {
+
     const [isDataRecieved, setIsDataRecieved] = useState(false);
     const [isAddContributorsVisible, setIsAddContributorsVisible] = useState(true);
     const [isFinalDocumentReady, setIsFinalDocumentReady] = useState(false);
@@ -17,6 +30,15 @@ function App() {
         products: []
     });
     const [finalDocument, setFinalDocument] = useState();
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    // user ID for attaching it to the document.
+    const [userId, setUserId] = useState("");
+
+    const handleLoginSuccess = (isLoggedIn) => {
+        setIsLoggedIn(isLoggedIn);
+        // do something with the userData, if needed
+    }
+
 
     useEffect(() => {
         let tempNamesHolder = [];
@@ -33,12 +55,14 @@ function App() {
 
     useEffect(() => {
         if (isDataRecieved) {
-            fetch('http://localhost:3000/', {
+            console.log(jsonState);
+            fetch('http://localhost:3001/', {
                 method: 'POST',
                 headers: {
                     'Accept': 'application/json, text/plain, */*',
                     'Content-Type': 'application/json'
                 },
+
                 body: JSON.stringify(jsonState),
             }) // response from BackEnd comes here
                 .then((res) => {
@@ -80,22 +104,105 @@ function App() {
         console.log(jsonState);
     }
 
+    // For - POST request using Postman
+    function handleFetchClick() {
+        getInfo();
+        setIsDataRecieved(true);
+    }
+
+    function logOut() {
+        logout();
+        setIsLoggedIn(false);
+    }
     return (
-        <div className="app-container">
-            {
-                isFinalDocumentReady
-                    ?
-                    <Result finalDocument={finalDocument} />
-                    :
-                    isAddContributorsVisible
-                        ?
-                        <AddContributors handlePayersState={handlePayersState} chooseComponent={chooseComponent} />
-                        :
-                        <AddProducts payers={payers} recieveData={recieveData} />
-            }
-            <button onClick={getInfo}> click me!</button>
-        </div>
-    )
+        <>
+            <BrowserRouter>
+                <div className="app-container">
+                    {/* <button onClick={handleFetchClick}> Postman - Fetch Data</button> */}
+                    {/* Routing - Start */}
+                    {/* navbar-test */}
+                    <nav className="navbar navbar-expand navbar-dark bg-dark">
+                        <Link to={"/"} className="navbar-brand">
+                            Box App
+                        </Link>
+                        <div className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <Link to={"/"} className="nav-link">
+                                    Home
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to={"/all-results"} className="nav-link">
+                                    View All Results
+                                </Link>
+                            </li>
+                        </div>
+
+                        <div className="navbar-nav ml-auto">
+                            {isLoggedIn ? (
+                                <>
+                                    <li className="nav-item">
+                                        <Link to={"/profile"} className="nav-link">
+                                            Profile
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link to={"/profile"} className="nav-link" onClick={logOut}>
+                                            Logout
+                                        </Link>
+                                    </li>
+                                </>
+                            ) : (
+                                <>
+                                    <li className="nav-item">
+                                        <Link to={"/login"} className="nav-link">
+                                            Login
+                                        </Link>
+                                    </li>
+
+                                    <li className="nav-item">
+                                        <Link to={"/register"} className="nav-link">
+                                            Sign Up
+                                        </Link>
+                                    </li>
+                                </>
+
+                            )}
+
+
+                        </div>
+                    </nav>
+                    {/* navbar-test-finished */}
+                    <Routes>
+
+                        <Route exact path="/" element=
+
+                            {
+                                isFinalDocumentReady
+                                    ?
+                                    <Result finalDocument={finalDocument} />
+                                    :
+                                    isAddContributorsVisible
+                                        ?
+                                        <AddContributors handlePayersState={handlePayersState} chooseComponent={chooseComponent} />
+                                        :
+                                        <AddProducts payers={payers} recieveData={recieveData} />
+                            }
+                        />
+                        {/* Add a Route component to render the AllResults component */}
+                        <Route exact path="/all-results" element={<AllResults />} />
+                        <Route exact path="/login" element={<LoginComponent onLoginSuccess={handleLoginSuccess} />} />
+                        <Route exact path="/register" element={<RegisterComponent />} />
+                        <Route exact path="/profile" element={<Profile />} />
+
+
+                    </Routes>
+                    {/* Routing Finished */}
+
+                </div>
+            </BrowserRouter>
+        </>
+    );
 }
 
 
