094517f6d3bb04583a20956f1c1f4519788651e6
Babak Abdolah Zadeh <babak.abdzadeh@gmail.com>
2023-05-25 18:12:02 +0200

    update the component

--- client/src/components/user/User-board.user.jsx
+++ client/src/components/user/User-board.user.jsx
@@ -1,20 +1,23 @@
 import React, { useState, useEffect } from "react";
+import Result from "../Result";
 import { getUserBoard } from "../../services/user.service";
 
 
 export default function UserBoard() {
-    const [content, setContent] = useState({});
+    const [content, setContent] = useState([]);
     useEffect(() => {
-        const content = getUserBoard()
-        setContent(content)
+        const content = getUserBoard().then(data => setContent(data)).catch(error => console.error(error));
 
     }, []);
 
     return (
-        <div className="container">
-            <header className="jumbotron">
-                <h3>{content.mes}</h3>
-            </header>
+        <div className="user-board-container">
+            {content.map((key) => (
+                <header className="jumbotron">
+
+                    < Result finalDocument={key} />
+                </header>
+            ))}
         </div>
     );
 }
