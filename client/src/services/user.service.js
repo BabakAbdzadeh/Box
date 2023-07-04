92fff94cdc47bf1b7b953b46e865f5c2bfced758
Babak Abdolah Zadeh <babak.abdzadeh@gmail.com>
2023-07-04 19:40:52 +0200

    Enhance GetUserBoard function, userID issue resolved

--- client/src/services/user.service.js
+++ client/src/services/user.service.js
@@ -1,11 +1,13 @@
 import authHeader from "./auth-header";
 import { getCurrentUser } from "./auth.service";
 
-const userId = getCurrentUser().id;
+
 const API_URL = "http://localhost:3001/api/user/";
 
 export async function getUserBoard() {
     try {
+        const user = getCurrentUser();
+        const userId = user ? user.id : null;
         const response = await fetch(API_URL + `results?id=${userId}`, {
             method: 'GET',
             headers: authHeader()
@@ -14,13 +16,13 @@ export async function getUserBoard() {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data = await response.json();
-        // TEST
         console.log("test" + data);
         return data;
     } catch (err) {
         console.error(err);
     }
 };
+
 export async function getAdminBoard() {
     try {
         const response = await fetch(API_URL + 'admin', {
