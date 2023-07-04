2e39dbb186ea4914181c6f465cc7059602472d59
Babak Abdolah Zadeh <babak.abdzadeh@gmail.com>
2023-07-04 19:32:23 +0200

    Implement array manipulation, content rendering, and CSS styling enhancements

--- client/src/components/user/Admin-board.user.jsx
+++ client/src/components/user/Admin-board.user.jsx
@@ -1,23 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { getAdminBoard } from "../../services/user.service";
 
-
 export default function AdminBoard() {
-    const [content, setContent] = useState({});
-    useEffect(() => {
-        getAdminBoard().then(data => setContent(data)).catch(error => console.error(error));
+    const [content, setContent] = useState([]);
 
+    useEffect(() => {
+        getAdminBoard()
+            .then(data => setContent(data))
+            .catch(error => console.error(error));
 
     }, []);
 
     return (
-        <div className="container">
+        <div className="board-container">
             <header className="jumbotron">
-                <h3>{content.message}</h3>
-
+                {content.map((item, index) => (
+                    <><h3 key={index}> user {index} : {item}</h3><br></br></>
+                ))}
             </header>
         </div>
     );
 }
-
-
