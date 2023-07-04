8a92debbbb67df719904c05b7bf6298405f7b2e8
Babak Abdolah Zadeh <babak.abdzadeh@gmail.com>
2023-07-04 19:49:38 +0200

    update the main component to work with the recent updates, fixes, and enhancements

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,236 +1,90 @@
 
 import React, { useState, useEffect } from 'react';
-
-import AddContributors from "./AddContributors";
-import AddProducts from './AddProducts.jsx';
-import Result from './Result'
+import BoxApp from './BoxApp';
 // React routing
-import AllResults from './AllResults';
 import LoginComponent from './auth/Login.auth';
 import RegisterComponent from './auth/Register.auth';
 import Profile from './user/Profile.user';
+import LogoutButton from './LogoutButton';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 // Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import { logout, getCurrentUser } from '../services/auth.service';
+import { getCurrentUser } from '../services/auth.service';
 
 
 
 
 function App() {
-
-    const [isDataRecieved, setIsDataRecieved] = useState(false);
-    const [isAddContributorsVisible, setIsAddContributorsVisible] = useState(true);
-    const [isFinalDocumentReady, setIsFinalDocumentReady] = useState(false);
-    const [payers, setPayers] = useState([]);
-    const [jsonState, setJSON] = useState({
-        names: "",
-        products: []
-    });
-    const [finalDocument, setFinalDocument] = useState();
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-    // user ID for attaching it to the document.
-    const [userId, setUserId] = useState("");
 
     const handleLoginSuccess = (isLoggedIn) => {
         setIsLoggedIn(isLoggedIn);
     }
-    // login check for refresh
+
     useEffect(() => {
         const user = getCurrentUser();
         if (user) {
             setIsLoggedIn(true);
         }
-    })
-
-    function logOut() {
-        logout();
-        setIsLoggedIn(false);
-    }
-
-    // to attach user's id to the document.
-    useEffect(() => {
-        const user = getCurrentUser();
-        const loggedUserID = user && user.id;
-        if (loggedUserID) {
-            setUserId(loggedUserID);
-        }
-    }, [isLoggedIn]);
-
-    useEffect(() => {
-        if (userId !== "") {
-            setJSON((json) => ({
-                ...json,
-                user: userId
-            }));
-        }
-
-    }, [userId]);
-
-
-    useEffect(() => {
-        let tempNamesHolder = [];
-        payers.forEach(payer => {
-            tempNamesHolder.push(payer.name);
-        })
-        setJSON(json => (
-            {
-                ...json,
-                names: tempNamesHolder
-            }
-        ));
-    }, [payers]);
-
-    useEffect(() => {
-        if (isDataRecieved) {
-            console.log(jsonState);
-            fetch('http://localhost:3001/api/data/', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json'
-                },
-
-                body: JSON.stringify(jsonState),
-            }) // response from BackEnd comes here
-                .then((res) => {
-                    console.log(res.status);
-                    return res.json();
-                })
-                .then((data) => {
-                    console.log(data)
-                    if (data) {
-                        // window.location.href = "/results";
-                        console.log(data);
-                        setFinalDocument(data);
-                        setIsFinalDocumentReady(true);
-                    }
-                });
-        }
-        // fetch()
-    }, [isDataRecieved])
-
-    function chooseComponent(boolian) {
-        // we want false after click
-        setIsAddContributorsVisible(boolian);
-    }
-    function handlePayersState(newState) {
-        setPayers(newState);
-
-    }
-    function recieveData(data) {
-        setJSON(prevValues => {
-            const updatedProductArray = [...prevValues.products, data];
-            return {
-                ...prevValues,
-                products: updatedProductArray
-            }
-        });
-        setIsDataRecieved(true);
-    }
-    function getInfo() {
-        console.log(jsonState);
-    }
-
-    // For - POST request using Postman
-    function handleFetchClick() {
-        getInfo();
-        setIsDataRecieved(true);
-    }
+    }, [])
 
     return (
-        <>
-            <BrowserRouter>
-                <div className="app-container">
-                    {/* <button onClick={handleFetchClick}> Postman - Fetch Data</button> */}
-                    {/* Routing - Start */}
-                    {/* navbar-test */}
-                    <nav className="navbar navbar-expand navbar-dark bg-dark">
-                        <Link to={"/"} className="navbar-brand">
-                            Box App
-                        </Link>
-                        <div className="navbar-nav mr-auto">
-                            <li className="nav-item">
-                                <Link to={"/"} className="nav-link">
-                                    Home
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to={"/all-results"} className="nav-link">
-                                    View All Results
-                                </Link>
-                            </li>
-                        </div>
+        <BrowserRouter>
+            <div className="app-container">
+                <nav className="navbar navbar-expand navbar-dark bg-dark">
+                    <Link to={"/"} className="navbar-brand">
+                        Box App
+                    </Link>
+                    <div className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <Link to={"/"} className="nav-link">
+                                Home
+                            </Link>
+                        </li>
 
-                        <div className="navbar-nav ml-auto">
-                            {isLoggedIn ? (
-                                <>
-                                    <li className="nav-item">
-                                        <Link to={"/profile"} className="nav-link">
-                                            Profile
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link to={"/profile"} className="nav-link" onClick={logOut}>
-                                            Logout
-                                        </Link>
-                                    </li>
-                                </>
-                            ) : (
-                                <>
-                                    <li className="nav-item">
-                                        <Link to={"/login"} className="nav-link">
-                                            Login
-                                        </Link>
-                                    </li>
+                    </div>
 
-                                    <li className="nav-item">
-                                        <Link to={"/register"} className="nav-link">
-                                            Sign Up
-                                        </Link>
-                                    </li>
-                                </>
+                    <div className="navbar-nav ml-auto">
+                        {isLoggedIn ? (
+                            <>
+                                <li className="nav-item">
+                                    <Link to={"/profile"} className="nav-link">
+                                        Profile
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <LogoutButton setIsLoggedIn={setIsLoggedIn} />
+                                </li>
+                            </>
+                        ) : (
+                            <>
+                                <li className="nav-item">
+                                    <Link to={"/login"} className="nav-link">
+                                        Login
+                                    </Link>
+                                </li>
 
-                            )}
+                                <li className="nav-item">
+                                    <Link to={"/register"} className="nav-link">
+                                        Sign Up
+                                    </Link>
+                                </li>
+                            </>
+                        )}
+                    </div>
+                </nav>
 
+                <Routes>
+                    <Route path="/" element={<BoxApp />} />
 
-                        </div>
-                    </nav>
-                    {/* navbar-test-finished */}
-                    <Routes>
-
-                        <Route exact path="/" element=
-
-                            {
-                                isFinalDocumentReady
-                                    ?
-                                    <Result finalDocument={finalDocument} />
-                                    :
-                                    isAddContributorsVisible
-                                        ?
-                                        <AddContributors handlePayersState={handlePayersState} chooseComponent={chooseComponent} />
-                                        :
-                                        <AddProducts payers={payers} recieveData={recieveData} />
-                            }
-                        />
-                        {/* Add a Route component to render the AllResults component */}
-                        <Route exact path="/all-results" element={<AllResults />} />
-                        <Route exact path="/login" element={<LoginComponent onLoginSuccess={handleLoginSuccess} />} />
-                        <Route exact path="/register" element={<RegisterComponent />} />
-                        <Route exact path="/profile" element={<Profile />} />
-
-
-                    </Routes>
-                    {/* Routing Finished */}
-
-                </div>
-            </BrowserRouter>
-        </>
+                    <Route path="/login" element={<LoginComponent onLoginSuccess={handleLoginSuccess} />} />
+                    <Route path="/register" element={<RegisterComponent />} />
+                    <Route path="/profile" element={<Profile />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
-
-// add id to the jsonState structor.
 export default App;
